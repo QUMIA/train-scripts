9a8708935eaf9eae66c39924cb4845318ff3c869
Peter Kok <kretep@users.noreply.github.com>
2023-12-25 12:39:43 +0100

    [SESSION] smaller-network-4x16x128

--- train.py
+++ train.py
@@ -44,20 +44,20 @@ if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
 
-# # start a new wandb run to track this script
-# wandb.init(
-#     # set the wandb project where this run will be logged
-#     project="qumia",
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="qumia",
 
-#     name=sessionLabel,
+    name=sessionLabel,
     
-#     # track hyperparameters and run metadata
-#     config={
-#         "learning_rate": 0.001,
-#         "architecture": "CNN",
-#         "epochs": 20,
-#     }
-# )
+    # track hyperparameters and run metadata
+    config={
+        "learning_rate": 0.001,
+        "architecture": "CNN",
+        "epochs": 20,
+    }
+)
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
