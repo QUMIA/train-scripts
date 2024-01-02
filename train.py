c7313ae34c6f43deadf9812f62ca150a8ea40485
Peter Kok <kretep@users.noreply.github.com>
2024-01-02 12:02:16 +0100

    [SESSION] larger-network-5x32x512

--- train.py
+++ train.py
@@ -106,7 +106,7 @@ def visualize_augmentations(dataset, idx=0, samples=10, cols=5):
 
 
 def create_model():
-    model = QUMIA_Model(1, image_size, 6, 32, 256)
+    model = QUMIA_Model(1, image_size, 5, 32, 512)
     model.to(device)
     return model
 
