5a136f2605b58b561898571cf338bdac51920e93
Peter Kok <kretep@users.noreply.github.com>
2024-06-19 20:16:12 +0200

    Revert "Refactor weighted loss"
    This reverts commit 486b570b48d4967d6daa55f27ff6d484d050357f.

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ import wandb
 
 from qumia_dataset import QUMIA_Dataset
 from qumia_model import QUMIA_Model
-from qumia_core import QUMIA_Trainer, train, validate, get_weighted_loss_function
+from qumia_core import QUMIA_Trainer, train, validate
 
 
 # Load environment variables from .env file
@@ -140,7 +140,24 @@ model = create_model()
 
 class_weights = torch.tensor([0.01195304017, 0.03527882809, 0.08486611199, 0.8679020198])
 class_weights = class_weights.to(device)
-criterion = get_weighted_loss_function(class_weights)
+
+def weighted_mse_loss(input, target):
+    assert input.shape == target.shape, "Input and target must have the same shape"
+
+    # Assign weights based on the target class
+    # This assumes targets are 1.0, 2.0, 3.0, and 4.0 for the classes
+    sample_weights = class_weights[target.long() - 1]
+
+    # Calculate MSE loss for each sample
+    mse = torch.nn.functional.mse_loss(input, target, reduction='none')
+
+    # Weight the MSE loss by the sample weights
+    weighted_mse = mse * sample_weights
+
+    # Return the mean loss
+    return weighted_mse.mean()
+
+criterion = weighted_mse_loss
 
 
 # Optimizer
