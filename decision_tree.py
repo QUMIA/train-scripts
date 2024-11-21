f1b58caee650a37488a3cb1afcb7debb320e80cb
Peter Kok <kretep@users.noreply.github.com>
2024-11-21 09:19:18 +0100

    Cleaning up

--- decision_tree.py
+++ decision_tree.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 import torch
+import os
 
 
 # Automatically reload modules
@@ -15,43 +16,71 @@ get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
 
+#data_dir = "/Users/peter/repos/qu/results/2024-06-13_17-02-36_masked-image-text/output"
+data_dir = "/Users/peter/repos/qu/results/2024-11-13_16-55-03_fix-clamping/output"
+
 # Read and concatenate the data sets (just use all of them for now)
-df1 = pd.read_csv("df_validation_predictions.csv")
-df2 = pd.read_csv("df_train_predictions.csv")
-#df3 = pd.read_csv("/projects/0/einf6214/output/2024-03-06_18-23-27_test-test-set/output/df_test_predictions.csv")
-df = pd.concat([df1, df2])
-print(df.shape)
-print(df.columns.values)
+df1 = pd.read_csv(os.path.join(data_dir, "df_train_predictions.csv"))
+df2 = pd.read_csv(os.path.join(data_dir, "df_validation_predictions.csv"))
+df3 = pd.read_csv(os.path.join(data_dir, "df_test_predictions.csv"))
+df_combined = pd.concat([df1, df2, df3])
+print(df_combined.shape)
+print(df1.shape, df2.shape, df3.shape)
+print(df_combined.columns.values)
+
+print(df_combined['h_score'].value_counts())
+
+print('patient count:', df_combined['anon_id'].nunique())
+print('exam count:', df_combined['exam_id'].nunique())
+
+# Remove diagnosis 99 (ignored in 2nd stage model)
+print(df_combined['diagnosis'].value_counts())
+df_combined = df_combined[df_combined['diagnosis'] != 99]
+
+print(df_combined.shape)
+
+# Drop duplicate entries based on 'patient_id'
+unique_patients = df_combined.drop_duplicates(subset='anon_id')
+total_count = unique_patients.shape[0]
+
+# Count the number of healthy (0) and diseased (non-0) patients
+diagnosis_counts = unique_patients['diagnosis'].apply(lambda x: 'Healthy' if x == 0 else 'Diseased').value_counts()
+print(diagnosis_counts)
+healthy_count = diagnosis_counts['Healthy']
+diseased_count = diagnosis_counts['Diseased']
+# percentages
+print(f"Total count: {total_count}")
+print(f"Healthy: {healthy_count} ({100*healthy_count/total_count:.2f}%)")
+print(f"Diseased: {diseased_count} ({100*diseased_count/total_count:.2f}%)")
 
 
 # Input
 input_column = 'muscle'
-input_value_column = 'h_score'
-print(df[input_column].value_counts())
-input_values = df[input_column].unique() # all muscles
+input_value_column = 'prediction'
+print(df_combined[input_column].value_counts())
+#input_values = df[input_column].unique() # all muscles
 #input_values = input_values[input_values != 'Extensors'] # remove Extensors
-#input_values = ['Biceps','RF','TA','GM','VL','Deltoid'] # six most common muscles
+input_values = ['Biceps','RF','TA','GM','VL','Deltoid'] # six most common muscles
 value_to_index = {value: idx for idx, value in enumerate(input_values)}
 print(value_to_index)
-
 
 # Output
 use_diagnosis = False
 
-# Remove diagnosis 99
-df = df[df['diagnosis'] != 99]
-print(df['diagnosis'].value_counts())
-
 
 # Aggregate the h_scores for each muscle
-df_agg = df.groupby(['exam_id', input_column]).agg({'prediction': 'max', 'h_score': 'max', 'diagnosis': 'first'}).reset_index()
+df_agg_train = df1.groupby(['exam_id', input_column]).agg({'prediction': 'max', 'h_score': 'max', 'diagnosis': 'first'}).reset_index()
+df_agg_val = df2.groupby(['exam_id', input_column]).agg({'prediction': 'max', 'h_score': 'max', 'diagnosis': 'first'}).reset_index()
+df_agg_test = df3.groupby(['exam_id', input_column]).agg({'prediction': 'max', 'h_score': 'max', 'diagnosis': 'first'}).reset_index()
 # df_agg['h_score'] = np.random.normal(2, 1.0, df_agg.shape[0])
-df_agg['h_score'] = 1.0
-df_agg.head(25)
+# df_agg['h_score'] = 1.0
+print(df_agg_train.head(5))
+print(df_agg_val.head(5))
+print(df_agg_test.head(5))
 
 
 def create_vectors(df):
-    input_vectors = {id: np.full(len(input_values), 1.0) for id in df['exam_id'].unique()}
+    input_vectors = {id: np.full(len(input_values), np.NaN) for id in df['exam_id'].unique()}
     label_vectors = {id: False for id in df['exam_id'].unique()}
 
     # Populate the vectors
@@ -72,33 +101,9 @@ def create_vectors(df):
     label_df = pd.DataFrame.from_dict(label_vectors, orient='index', columns=['diagnosis'])
     return (input_df, label_df)
 
-def create_max_vectors(df):
-    n = 4
-    input_vectors = {id: np.full(n, np.nan) for id in df['exam_id'].unique()}
-    label_vectors = {id: -1 for id in df['exam_id'].unique()}
-
-    grouped = df.groupby('exam_id')
-
-    # Iterate and perform operations
-    for name, group in grouped:
-        # print(f"Group: {name}, Value count: {group['prediction'].count()}")
-        # print(group.nlargest(n, input_value_column)[input_value_column].values)
-        if group.shape[0] >= n:
-            largest_n = group.nlargest(n, input_value_column)[input_value_column].values
-            for i in range(n):
-                input_vectors[name][i] = largest_n[i]
-            label_vectors[name] = group['diagnosis'].values[0]
-        else:
-            # remove from dictionary
-            del input_vectors[name]
-            del label_vectors[name]
-
-    # Convert the dictionary to a DataFrame for easy handling/viewing
-    input_df = pd.DataFrame.from_dict(input_vectors, orient='index', columns=[f'{input_value_column}_{i}' for i in range(n)])
-    label_df = pd.DataFrame.from_dict(label_vectors, orient='index', columns=['diagnosis'])
-    return (input_df, label_df)
-
-input_df, label_df = create_vectors(df_agg)
+input_train_df, label_train_df = create_vectors(df_agg_train)
+input_val_df, label_val_df = create_vectors(df_agg_val)
+input_test_df, label_test_df = create_vectors(df_agg_test)
 
 # Handling missing values in 'input_df' (simple imputation with mean of each column)
 # input_df = input_df.apply(lambda x: x.fillna(x.mean()), axis=1)
@@ -106,129 +111,26 @@ input_df, label_df = create_vectors(df_agg)
 #input_df_filled = pd.DataFrame(0, index=np.arange(input_df.shape[0]), columns=input_df.columns)
 #print(input_df_filled.head(10))
 
-print(input_df.shape, label_df.shape)
-display(input_df.head(20))
-display(label_df.head(20))
-
-# input_df, label_df = create_max_vectors(df_agg)
-# display(input_df.head(10))
-# display(label_df.head(10))
+print(input_train_df.shape, label_train_df.shape)
+print(input_val_df.shape, label_val_df.shape)
+print(input_test_df.shape, label_test_df.shape)
+# display(input_df.head(20))
+# display(label_df.head(20))
 
 
 # Splitting data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(input_df, label_df, test_size=0.2, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(input_df, label_df, test_size=0.2, random_state=42)
 
-# Initializing and training the Decision Tree Classifier
-clf = DecisionTreeClassifier(random_state=42, max_depth=10, class_weight='balanced')
-clf.fit(X_train, y_train)
-
-def print_accuracy(model, X, y):
-    predictions = model.predict(X)
-    accuracy = accuracy_score(y, predictions)
-    print(f"Accuracy: {accuracy}")
-
-print_accuracy(clf, X_train, y_train)
-print_accuracy(clf, X_test, y_test)
-print(f"Maximum depth of the tree: {clf.tree_.max_depth}")
-print(f"Total number of nodes: {clf.tree_.node_count}")
-print(f"Number of leaves: {clf.tree_.n_leaves}")
+# Convert 
 
 
-# Accuracy: 0.7378917378917379
-# Accuracy: 0.4431818181818182
-# Maximum depth of the tree: 10
-# Total number of nodes: 143
-# Number of leaves: 72
-
-# Without diagnosis 99
-# Accuracy: 0.7670807453416149
-# Accuracy: 0.4691358024691358
-# Maximum depth of the tree: 10
-# Total number of nodes: 137
-# Number of leaves: 69
-
-# Without extensor muscles
-# Accuracy: 0.7670807453416149
-# Accuracy: 0.4567901234567901
-# Maximum depth of the tree: 10
-# Total number of nodes: 141
-# Number of leaves: 71
-
-# Before fillna(mean)
-# Accuracy: 0.9004702194357367
-# Accuracy: 0.70625
-# Maximum depth of the tree: 10
-# Total number of nodes: 273
-# Number of leaves: 137
-
-# After fillna(mean)
-# Accuracy: 0.8612852664576802
-# Accuracy: 0.7
-# Maximum depth of the tree: 10
-# Total number of nodes: 181
-# Number of leaves: 91
-
-# Accuracy: 0.8934169278996865
-# Accuracy: 0.6875
-# Maximum depth of the tree: 10
-# Total number of nodes: 325
-# Number of leaves: 163
-
-# Accuracy: 0.8103448275862069
-# Accuracy: 0.696875
-# Maximum depth of the tree: 10
-# Total number of nodes: 255
-# Number of leaves: 128
-
-
-label_values = label_df['diagnosis'].unique()
+label_values = label_train_df['diagnosis'].unique()
 label_values = [str(x) for x in label_values]
 print(input_values, label_values)
 
 
-from sklearn.tree import plot_tree
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(100,50))
-plot_tree(clf, filled=True, rounded=True, class_names=label_values, feature_names=input_values)
-plt.show()
-
-
 print (X_train)
 print (y_train)
-
-
-from sklearn.metrics import f1_score
-
-def print_f1_score(X_train, Y_train, X_test, y_test):
-    y_train_pred = mlp_classifier.predict(X_train)
-    y_test_pred = mlp_classifier.predict(X_test)
-    f1_score_train = f1_score(y_train, y_train_pred, average='weighted')
-    f1_score_test = f1_score(y_test, y_test_pred, average='weighted')
-    print(f"F1 score (train): {f1_score_train:.2f}")
-    print(f"F1 score (test): {f1_score_test:.2f}")
-
-
-# from sklearn.neural_network import MLPClassifier
-# from sklearn.model_selection import train_test_split
-
-
-# # Initialize the MLPClassifier
-# mlp_classifier = MLPClassifier(hidden_layer_sizes=(50,), activation='relu', solver='adam', max_iter=500)
-
-# # Train the model
-# mlp_classifier.fit(X_train, y_train)
-
-# # Evaluate the model
-# print_f1_score(X_train, y_train, X_test, y_test)
-
-
-# import shap
-
-# # explain all the predictions in the test set
-# explainer = shap.KernelExplainer(sklearn.nn.predict_proba, X_train)
-# shap_values = explainer.shap_values(X_test)
-# shap.force_plot(explainer.expected_value[0], shap_values[..., 0], X_test)
 
 
 print(X_train.shape, X_test.shape)
@@ -255,10 +157,11 @@ def df_to_masked_array(df):
 
     return two_channel_array
 
-X_train_masked = df_to_masked_array(X_train)
+X_train_masked = df_to_masked_array(input_train_df)
 print(X_train_masked.shape)
-#print(X_train_masked[0:10])
-X_test_masked = df_to_masked_array(X_test)
+X_val_masked = df_to_masked_array(input_val_df)
+print(X_val_masked.shape)
+X_test_masked = df_to_masked_array(input_test_df)
 print(X_test_masked.shape)
 
 
@@ -266,14 +169,18 @@ def boolean_df_to_int_array(df):
     return df.astype(int).to_numpy()
 
 if use_diagnosis:
-    y_train_int = y_train.astype(int).to_numpy()
+    y_train_int = label_train_df.astype(int).to_numpy()
     print(y_train_int[0:10])
-    y_test_int = y_test.astype(int).to_numpy()
+    y_val_int = label_val_df.astype(int).to_numpy()
+    print(y_val_int[0:10])
+    y_test_int = label_test_df.astype(int).to_numpy()
     print(y_test_int[0:10])
 else:
-    y_train_int = boolean_df_to_int_array(y_train)
+    y_train_int = boolean_df_to_int_array(label_train_df)
     print(y_train_int[0:10])
-    y_test_int = boolean_df_to_int_array(y_test)
+    y_val_int = boolean_df_to_int_array(label_val_df)
+    print(y_val_int[0:10])
+    y_test_int = boolean_df_to_int_array(label_test_df)
     print(y_test_int[0:10])
 
 
@@ -281,12 +188,16 @@ from torch.utils.data import Dataset, DataLoader
 
 X_train_tensor = torch.tensor(X_train_masked, dtype=torch.float).permute(0, 2, 1)  # Swap the last two dimensions
 y_train_tensor = torch.tensor(y_train_int, dtype=torch.long)
+X_val_tensor = torch.tensor(X_val_masked, dtype=torch.float).permute(0, 2, 1)  # Swap the last two dimensions
+y_val_tensor = torch.tensor(y_val_int, dtype=torch.long)
 X_test_tensor = torch.tensor(X_test_masked, dtype=torch.float).permute(0, 2, 1)  # Swap the last two dimensions
 y_test_tensor = torch.tensor(y_test_int, dtype=torch.long)
 
 # Create DataLoader for batching
 train_data = torch.utils.data.TensorDataset(X_train_tensor, y_train_tensor)
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+val_data = torch.utils.data.TensorDataset(X_val_tensor, y_val_tensor)
+val_loader = DataLoader(val_data, batch_size=32, shuffle=False)
 test_data = torch.utils.data.TensorDataset(X_test_tensor, y_test_tensor)
 test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 
@@ -301,11 +212,20 @@ from torchsummary import summary
 vector_length = len(input_values)
 num_classes = len(label_values) if use_diagnosis else 1
 model = MaskedClassifier(vector_length, num_classes)
-criterion = nn.CrossEntropyLoss() if use_diagnosis else nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss() if use_diagnosis else nn.BCEWithLogitsLoss(pos_weight=torch.tensor([0.45]))
+optimizer = optim.Adam(model.parameters(), lr=0.005)
 
 # print model summary
 summary(model, (2, vector_length))
+
+# Calculate losses
+total_loss = 0.0
+for inputs, labels in train_loader:
+    outputs = model(inputs)
+    target = labels.squeeze() if use_diagnosis else labels.float()
+    loss = criterion(outputs, target)
+    total_loss += loss.item()
+print(f'Initial loss: {total_loss/len(train_loader)}')
 
 # Training the model
 num_epochs = 20  # Set the number of epochs
@@ -321,8 +241,17 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
     
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+    # Calculate loss on validation set
+    with torch.no_grad():
+        val_loss = 0.0
+        for inputs, labels in val_loader:
+            outputs = model(inputs)
+            target = labels.squeeze() if use_diagnosis else labels.float()
+            loss = criterion(outputs, target)
+            val_loss += loss.item()
+        val_loss /= len(val_loader)
 
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}, Val Loss: {val_loss:.4f}')
 
 
 # Evaluate the model
@@ -342,8 +271,8 @@ from sklearn.metrics import f1_score
 model.eval()
 
 # Store all the predictions and true labels
-all_predictions = []
-all_labels = []
+test_predictions = []
+test_labels = []
 correct_predictions = 0
 total_predictions = 0
 
@@ -356,8 +285,8 @@ with torch.no_grad():
             _, predicted_classes = torch.max(outputs, 1)
             
             # Collect the predictions and true labels
-            all_predictions.extend(predicted_classes.tolist())
-            all_labels.extend(labels.tolist())
+            test_predictions.extend(predicted_classes.tolist())
+            test_labels.extend(labels.tolist())
 
             correct_predictions += (predicted_classes == labels.squeeze()).sum().item()
             total_predictions += labels.size(0)
@@ -367,21 +296,22 @@ with torch.no_grad():
             # Apply sigmoid to the outputs to get the probabilities
             probs = torch.sigmoid(outputs.squeeze())  # Squeeze to remove unnecessary dimensions
             # Convert probabilities to binary predictions using a threshold of 0.5
-            predictions = (probs >= 0.5).long()  # Convert to long to match labels type
+            
+            #predictions = (probs >= 0.5).long()  # Convert to long to match labels type
             
             # Collect the predictions and true labels (for f1 score)
-            all_predictions.extend(predictions.tolist())
-            all_labels.extend(labels.tolist())
+            test_predictions.extend(probs.tolist())
+            test_labels.extend(labels.tolist())
 
             # Compare predictions to true labels (for accuracy)
-            correct_predictions += (predictions == labels.squeeze()).sum().item()
-            total_predictions += labels.size(0)
+            #correct_predictions += (predictions == labels.squeeze()).sum().item()
+            #total_predictions += labels.size(0)
 
 if use_diagnosis:
     # Calculate the F1 score (choose 'macro', 'micro', or 'weighted' as per your requirement)
-    f1_macro = f1_score(all_labels, all_predictions, average='macro')
-    f1_micro = f1_score(all_labels, all_predictions, average='micro')
-    f1_weighted = f1_score(all_labels, all_predictions, average='weighted')
+    f1_macro = f1_score(test_labels, test_predictions, average='macro')
+    f1_micro = f1_score(test_labels, test_predictions, average='micro')
+    f1_weighted = f1_score(test_labels, test_predictions, average='weighted')
 
     print(f'F1 Score (Macro): {f1_macro:.4f}')
     print(f'F1 Score (Micro): {f1_micro:.4f}')
@@ -391,25 +321,92 @@ if use_diagnosis:
     # all_classes = np.array(label_values)
     # unique_labels = np.unique(np.concatenate((all_classes, all_predictions)))
     # print(unique_labels)
-    report = classification_report(all_labels, all_predictions)
+    report = classification_report(test_labels, test_predictions)
     print(report)
     
 else:
+    pass
     # Calculate the F1 score
-    f1 = f1_score(all_labels, all_predictions, average='binary')  # 'binary' for binary classification tasks
-    print(f'F1 Score: {f1:.4f}')
+    #f1 = f1_score(all_labels, all_predictions, average='binary')  # 'binary' for binary classification tasks
+    #print(f'F1 Score: {f1:.4f}')
 
-print(f'Accuracy: {correct_predictions / total_predictions:.4f}')
-
-
-import torch
-if torch.backends.mps.is_available():
-    mps_device = torch.device("mps")
-    x = torch.ones(1, device=mps_device)
-    print (x)
-else:
-    print ("MPS device not found.")
+#print(f'Accuracy: {correct_predictions / total_predictions:.4f}')
 
 
+from sklearn.metrics import confusion_matrix
 
+# Assuming all_predictions and all_labels are your predicted and true labels, respectively
+
+# Generate the confusion matrix
+# cm = confusion_matrix(all_labels, all_predictions)
+
+# # Print the confusion matrix
+# print("Confusion Matrix:")
+# col_labels = ['', 'Pred 0', 'Pred 1']
+# pred_labels = ['True 0', 'True 1']
+# for i, label in enumerate(col_labels):
+#     print(f"{label:>8}", end=" ")
+# print()
+# for i, row in enumerate(cm):
+#     print(f"{pred_labels[i]:>8}", end=" ")
+#     for val in row:
+#         print(f"{val:>8}", end=" ")
+#     print()
+
+
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+import matplotlib.pyplot as plt
+
+
+# Compute Precision-Recall and plot curve, based on test_predictions and test_labels
+y_test = np.array(test_labels)
+y_score = np.array(test_predictions)
+
+precision, recall, _ = precision_recall_curve(y_test, y_score)
+average_precision = average_precision_score(y_test, y_score)
+
+# Plot
+plt.clf()
+plt.plot(recall, precision, label='Precision-Recall curve')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.ylim([0.0, 1.05])
+plt.xlim([0.0, 1.0])
+plt.title('Precision-Recall, AUC={0:0.2f}'.format(average_precision))
+plt.legend(loc="lower left")
+plt.show()
+
+
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
+
+# # Assuming you have your features in X and target in y
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Binarize the output
+y_test = label_binarize(y_test, classes=[0, 1])
+
+# Train your classifier and predict probabilities
+# clf.fit(X_train, y_train)
+y_score = clf.predict_proba(X_test)
+
+# Compute ROC curve and ROC area for each class
+fpr, tpr, _ = roc_curve(y_test, y_score[:, 1])
+roc_auc = auc(fpr, tpr)
+
+# Plot
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic example')
+plt.legend(loc="lower right")
+plt.show()
 
